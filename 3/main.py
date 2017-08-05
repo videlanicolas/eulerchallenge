07d47cfe2f6c8a374948a48ad727dc24cd98e548
Nicolas Videla <videlanicolas@gmail.com>
2017-08-05 15:49:32 -0300

    Challenge 3

--- 3/main.py
+++ 3/main.py
@@ -18,21 +18,28 @@ def get_prime(_min,_max):
 			return prime
 
 def get_factor(n):
-	if is_prime(n):
-		return n
-	elif n%2 == 0:
+	if pow(2,n-1,n) == 1:
+		if is_prime(n):
+			return n
+	if n%2 == 0:
 		return 2
 	a = 100
 	count = 0
+	#Check if first 100 primes 
+	for prime in  [3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541]:
+		if gcd(n,prime) != 1:
+			return prime
 	while True:
 		factor = get_prime(1,a)
 		if gcd(n,factor) != 1:
 			return factor
-		if 23 < count < 122:
+		if 0 <= count < 30:
+			a = 100
+		elif 30 <= count < 122:
 			a = 1000
-		elif 123 < count < 963:
+		elif 122 <= count < 963:
 			a = 10000
-		elif 964 < count < 7763:
+		elif 963 <= count < 7763:
 			a = 100000
 		else:
 			a = int(n/2)
